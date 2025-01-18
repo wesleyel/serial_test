47935963adb18d34690e46e17464c162462feb18
Wesleyel <48174882+wesleyel@users.noreply.github.com>
2025-01-18 02:38:17 +0000

    Refactor LineCodec implementation in main.rs to correctly handle CRLF line endings and remove HexCodec. Update main function to utilize LineCodec for reading lines from the serial port.

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,9 @@ impl Decoder for LineCodec {
     type Error = io::Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let newline = src.as_ref().iter().position(|b| *b == b'\n');
+        let newline = src.as_ref().windows(2).position(|w| w == b"\r\n");
         if let Some(n) = newline {
-            let line = src.split_to(n + 1);
+            let line = src.split_to(n + 2);
             return match str::from_utf8(line.as_ref()) {
                 Ok(s) => Ok(Some(s.to_string())),
                 Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Invalid String")),
@@ -35,37 +35,6 @@ impl Encoder<String> for LineCodec {
     }
 }
 
-struct HexCodec;
-
-impl Decoder for HexCodec {
-    type Item = String;
-    type Error = io::Error;
-
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let hex = src
-            .as_ref()
-            .iter()
-            .map(|b| {
-                let char_representation = if b.is_ascii_graphic() {
-                    *b as char
-                } else {
-                    '.'
-                };
-                format!("{}({:02x})", char_representation, b)
-            })
-            .collect::<String>();
-        Ok(Some(hex))
-    }
-}
-
-impl Encoder<String> for HexCodec {
-    type Error = io::Error;
-
-    fn encode(&mut self, _item: String, _dst: &mut BytesMut) -> Result<(), Self::Error> {
-        Ok(())
-    }
-}
-
 #[tokio::main]
 async fn main() -> tokio_serial::Result<()> {
     let mut args = env::args();
@@ -76,16 +45,11 @@ async fn main() -> tokio_serial::Result<()> {
     port.set_exclusive(false)
         .expect("Unable to set serial port exclusive to false");
 
-    // let mut reader = LineCodec.framed(port);
+    let mut reader = LineCodec.framed(port);
 
-    // while let Some(line_result) = reader.next().await {
-    //     let line = line_result.expect("Failed to read line");
-    //     println!("{}", line);
-    // }
-    let mut reader = HexCodec.framed(port);
-    while let Some(hex_result) = reader.next().await {
-        let hex = hex_result.expect("Failed to read hex");
-        println!("{}", hex);
+    while let Some(line_result) = reader.next().await {
+        let line = line_result.expect("Failed to read line");
+        println!("{}", line);
     }
     Ok(())
 }
